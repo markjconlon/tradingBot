cce00818a6f5dfa4148e0d1605d3a0f89849b3f3
markjconlon <markjconlon@gmail.com>
2017-10-13 19:56:35 -0400

    basic trade api call, will figure out encryption on its own first

--- app/assets/javascripts/trades.js
+++ app/assets/javascripts/trades.js
@@ -3,3 +3,9 @@ $(document).ready(function(){
     e.preventDefault;
   })
 });
+
+// url= https://api.liqui.io
+// signature = nonce + command as a string then sign with secret key
+// compute the HMAC with hmac, string, secret key
+// hex digest?
+//
