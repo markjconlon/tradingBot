bee83e4dc1cbf59cfe57919d1bd742353d6e2017
markjconlon <markjconlon@gmail.com>
2017-11-07 15:15:36 -0500

    added environment variables to application.rb

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,7 @@
 require_relative 'boot'
-
 require 'rails/all'
+
+
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -9,6 +10,13 @@ Bundler.require(*Rails.groups)
 module TradingBot
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
+    config.before_configuration do
+      env_file = File.join(Rails.root, 'config', 'local_env.yml')
+      YAML.load(File.open(env_file)).each do |key, value|
+        ENV[key.to_s] = value
+      end if File.exists?(env_file)
+    end
+    
     config.load_defaults 5.1
 
     # Settings in config/environments/* take precedence over those specified here.
