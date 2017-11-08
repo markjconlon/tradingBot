e44aa23f4d8266a5a194742ae3e6901f249f8cad
markjconlon <markjconlon@gmail.com>
2017-11-08 10:09:54 -0500

    removed unnecessary sleep

--- app/controllers/trades_controller.rb
+++ app/controllers/trades_controller.rb
@@ -8,10 +8,9 @@ class TradesController < ApplicationController
   end
 
   def check_trades
-    until Trade.all.count > 3
+    until Trade.all.count > 4
       liqui_response = HTTParty.get('https://api.liqui.io/api/3/depth/eth_btc?limit=10')
       poloniex_response = HTTParty.get('https://poloniex.com/public?command=returnOrderBook&currencyPair=BTC_ETH&depth=10')
-      sleep 2
       puts liqui_response.body
       puts poloniex_response.body
       puts "//////////"
