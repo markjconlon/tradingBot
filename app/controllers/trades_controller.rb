52f15732ca8afdd082677c4aed4f9aaab27c7a43
markjconlon <markjconlon@gmail.com>
2017-11-08 10:32:15 -0500

    pulled count first so I don't have to manually updte for the until loop, removed printing the respnses to the console

--- app/controllers/trades_controller.rb
+++ app/controllers/trades_controller.rb
@@ -8,11 +8,11 @@ class TradesController < ApplicationController
   end
 
   def check_trades
-    until Trade.all.count > 4
+    count = Trade.all.count
+    until Trade.all.count > count
       liqui_response = HTTParty.get('https://api.liqui.io/api/3/depth/eth_btc?limit=10')
       poloniex_response = HTTParty.get('https://poloniex.com/public?command=returnOrderBook&currencyPair=BTC_ETH&depth=10')
-      puts liqui_response.body
-      puts poloniex_response.body
+      
       puts "//////////"
       puts Trade.all.count
       puts "/////////"
