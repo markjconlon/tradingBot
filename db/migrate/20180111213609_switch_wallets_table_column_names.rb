816bf9c5a88f62303e15c569a8f5377a12acf8d4
markjconlon <markjconlon@gmail.com>
2018-01-11 16:48:28 -0500

    switched wallets table btc columns to OMG

--- db/migrate/20180111213609_switch_wallets_table_column_names.rb
+++ db/migrate/20180111213609_switch_wallets_table_column_names.rb
@@ -1,8 +1,8 @@
 class SwitchWalletsTableColumnNames < ActiveRecord::Migration[5.1]
   def change
-    remove_column :trades, :liqui_btc, :float
-    remove_column :trades, :poloniex_btc, :float
-    add_column :trades, :liqui_omg, :float
-    add_column :trades, :poloniex_omg, :float
+    remove_column :wallets, :liqui_btc, :float
+    remove_column :wallets, :poloniex_btc, :float
+    add_column :wallets, :liqui_omg, :float
+    add_column :wallets, :poloniex_omg, :float
   end
 end
